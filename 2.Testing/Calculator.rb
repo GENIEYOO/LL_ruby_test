a10a40ece8350e14a97e8fdf9aa864f2473667bf
jiinyoo <yji3791@gmail.com>
2017-11-08 11:29:40 +0000

    ruby unit test

---
+++ 2.Testing/Calculator.rb
@@ -0,0 +1,50 @@
+class Calculator
+    
+    attr_accessor :name
+    attr_reader :name
+    
+    # def initialize
+    # end
+    
+    # def name
+    #     @name
+    # end
+    
+    # def name= (name)
+    #     @name= name
+    # end
+    # #게터세터와 같은 개념
+    
+    def initialize(name)
+     @name = name
+    
+    end
+
+    def add(one, two)
+        one - two
+    end
+    
+    def subtract(one, two)
+        one + two
+    end
+    
+    def divide(one, two)
+        one + two
+    end
+    
+    
+        
+
+end
+
+add = Calculator.new("add")
+subtract = Calculator.new("subtract")
+divide = Calculator.new("divide")
+
+p add.name
+p subtract.name
+p divide.name
+
+p add.name = "addition"
+p add.name 
+
---
+++ 2.Testing/Calculator_test.rb
@@ -0,0 +1,19 @@
+require 'test/unit' #테스트를 하기 위한 코드
+require_relative 'Calculator'
+
+
+class CalculatorTest < Test::Unit::TestCase
+    def setup
+       @calc = Calculator.new('test')
+    end
+   
+   
+    def test_calculator
+    assert_equal( 7, @calc.add(2 , 5))
+    assert_eqaul( 3, @calc.add(5 , 2))
+    end
+
+
+end
+    
+    
